eec0ab2e713d3b65fa71bed4d77c0277bd2276c3
f0rx <ejike.br@gmail.com>
2019-09-07 12:04:05 +0100

    Optimize Code

--- app/src/main/java/com/sample/newfeatured/ActInterface.java
+++ app/src/main/java/com/sample/newfeatured/ActInterface.java
@@ -9,20 +9,11 @@ import android.widget.Toast;
 import com.google.android.material.snackbar.BaseTransientBottomBar;
 import com.google.android.material.snackbar.Snackbar;
 
-import java.util.StringJoiner;
-
 public interface ActInterface {
     // Google Ad Unit - ca-app-pub-3940256099942544/1033173712
-//    String INTERSTITIAL_AD_1 = "ca-app-pub-5022917256332506/3637505299";
-//    String INTERSTITIAL_AD_2 = "ca-app-pub-5022917256332506/1818099119";
-//    String INTERSTITIAL_AD_3 = "ca-app-pub-5022917256332506/9365416642";
-//    String INTERSTITIAL_AD_4 = "ca-app-pub-5022917256332506/6739253307";
-//    String INTERSTITIAL_AD_5 = "ca-app-pub-5022917256332506/9173844958";
 
     String TAG = "new-feat-tag";
     String __BANNER_SEARCH_STRING = "Start Banner";
-
-//    String AD_APP_ID = new Activity().getString(R.string.ad_app_id);
 
     Handler adHandler = new Handler();
     long BANNER_REFRESH_DELAY = 20000;
